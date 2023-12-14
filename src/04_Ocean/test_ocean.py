51a246790628f11bb27c32865a0948c8cc8ea76c
Ganzha Eduard <edik.ganzha.05@gmail.com>
2023-12-14 15:11:19 +0900

    Сделал test_ocean

--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -1,22 +1,22 @@
+import pytest
+
 from ocean import Ocean
 
 
 class TestOcean:
+    init_state = [[0, 2, 0, 1, 3],
+                  [0, 2, 0, 3, 3],
+                  [0, 2, 2, 2, 1],
+                  [2, 1, 2, 0, 3],
+                  [3, 3, 1, 3, 3]]
 
-    def __init__(self):
-        self.init_state = [[0, 2, 0, 1, 3],
-                           [0, 2, 0, 3, 3],
-                           [0, 2, 2, 2, 1],
-                           [2, 1, 2, 0, 3],
-                           [3, 3, 1, 3, 3]]
+    next_state = [[0, 0, 0, 1, 3],
+                  [2, 2, 0, 3, 3],
+                  [2, 0, 0, 2, 1],
+                  [0, 1, 2, 2, 3],
+                  [0, 0, 1, 3, 3]]
 
-        self.next_state = [[0, 0, 0, 1, 3],
-                           [2, 2, 0, 3, 3],
-                           [2, 0, 0, 2, 1],
-                           [0, 1, 2, 2, 3],
-                           [0, 0, 1, 3, 3]]
-
-        self.ocean = Ocean(self.init_state)
+    ocean = Ocean(init_state)
 
     def test_ocean_init(self):
         assert str(self.ocean) == "\n".join(["".join(str(el) for el in row) for row in self.init_state])
