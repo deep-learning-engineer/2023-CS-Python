ae79d5d0a297dbcf78c0926606b0dbd169cc5f1d
Ganzha Eduard <edik.ganzha.05@gmail.com>
2023-12-14 11:55:06 +0900

    Сделал Ocean

--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+
 from typing import List
 
 
@@ -15,18 +17,51 @@ class Ocean:
         return f"{self.__class__.__name__}({self.state!r})"
 
     def gen_next_quantum(self) -> "Ocean":
-        raise NotImplementedError
+        num_rows, num_cols = len(self.state), len(self.state[0])
+        new_state = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+        old_state = np.pad(np.array(self.state.copy()), 1)
+
+        for row in range(1, num_rows+1):
+            for col in range(1, num_cols+1):
+                animals = {i: 0 for i in range(4)}
+
+                for step_x in [-1, 0, 1]:
+                    for step_y in [-1, 0, 1]:
+                        if abs(step_x) + abs(step_y) != 0:
+                            animals[old_state[row+step_x][col+step_y]] += 1
+                        else:
+                            continue
+
+                if (old_state[row][col] == 2) and ((animals[2] >= 4) or (animals[2] <= 1)):
+                    new_state[row-1][col-1] = 0
+
+                elif (old_state[row][col] == 3) and ((animals[3] >= 4) or (animals[3] <= 1)):
+                    new_state[row-1][col-1] = 0
+
+                elif old_state[row][col] == 0:
+                    if animals[2] == 3:
+                        new_state[row-1][col-1] = 2
+                    elif animals[3] == 3:
+                        new_state[row-1][col-1] = 3
+                    else:
+                        new_state[row-1][col-1] = old_state[row][col]
+
+                else:
+                    new_state[row-1][col-1] = old_state[row][col]
+
+        self.state = new_state
+        return self.state
 
 
 if __name__ == "__main__":
     n_quantums = int(sys.stdin.readline())
     n_rows, n_clms = (int(i) for i in sys.stdin.readline().split())
-    init_state = []
+    init_state_ = []
     for i in range(n_rows):
         line = [int(i) for i in sys.stdin.readline().split()]
-        init_state.append(line)
+        init_state_.append(line)
 
-    ocean = Ocean(init_state=init_state)
+    ocean = Ocean(init_state=init_state_)
     for _ in range(n_quantums):
         ocean = ocean.gen_next_quantum()
     print(ocean)
--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -1,14 +1,29 @@
 from ocean import Ocean
 
 
-def test_ocean_init():
-    Ocean
-    assert False
+class TestOcean:
 
+    def __init__(self):
+        self.init_state = [[0, 2, 0, 1, 3],
+                           [0, 2, 0, 3, 3],
+                           [0, 2, 2, 2, 1],
+                           [2, 1, 2, 0, 3],
+                           [3, 3, 1, 3, 3]]
 
-def test_ocean_repr():
-    assert False
+        self.next_state = [[0, 0, 0, 1, 3],
+                           [2, 2, 0, 3, 3],
+                           [2, 0, 0, 2, 1],
+                           [0, 1, 2, 2, 3],
+                           [0, 0, 1, 3, 3]]
 
+        self.ocean = Ocean(self.init_state)
 
-def test_ocean_step():
-    assert False
+    def test_ocean_init(self):
+        assert str(self.ocean) == "\n".join(["".join(str(el) for el in row) for row in self.init_state])
+
+    def test_ocean_repr(self):
+        assert self.ocean.__repr__() == f"Ocean({self.init_state!r})"
+
+    def test_ocean_step(self):
+        self.ocean.gen_next_quantum()
+        assert str(self.ocean) == "\n".join(["".join(str(el) for el in row) for row in self.next_state])
